67dc6aef1e5da249c30cf9a8614f98061d745768
HaoZeke <rohit.goswami@aol.com>
2018-05-04 03:40:43 +0530

    main: Update formspree address

--- src/components/Main.js
+++ src/components/Main.js
@@ -156,7 +156,7 @@ class Main extends React.Component {
 
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Contact</h2>
-          <form method="POST" action="https://formspree.io/hbtuMunBot@gmail.com">
+          <form method="POST" action="https://formspree.io/admin@aquick.review">
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
